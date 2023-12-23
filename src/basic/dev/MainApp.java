6b2deb2f9f88ab78fef5a500536787d9a63f57c9
TruongAN7023 <154291936+TruongAN7023@users.noreply.github.com>
2023-12-23 10:17:46 +0700

    Add files via upload

--- src/basic/dev/MainApp.java
+++ src/basic/dev/MainApp.java
@@ -4,7 +4,40 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// KBSinhVien
+				String studentName = "Tran Viet Truong An";
+				int studentCode = 2360212;
+				long Card = 123456789;
+				String nameClass = "23CD";
+				String nameSchool = "HueIC";
+				float DTB = 9f;
+				// KBCongNhan
+				String workerName = "Tran Dung";
+				String Address = "Trieu Son Dong";
+				long Phone = 78278244;
+				float Wage = 9f;
+				// KBXe
+				String carName = "AB ";
+				String CarCompany = "AB";
+				String valueCar = "50 million";
+				String Color = "Black";
+				// SV
+				System.out.println("Ho va ten SV: " + studentName);
+				System.out.println("Ma SV: " + studentCode);
+				System.out.println("CCCD: " + Card);
+				System.out.println("Lop: " + nameClass);
+				System.out.println("Truong: " + nameSchool);
+				System.out.println("DTB: " + DTB);
+				// CD
+				System.out.println("Ho ten Cong nhan: " + workerName);
+				System.out.println("Dia chi: " + Address);
+				System.out.println("So dien thoai: " + Phone);
+				System.out.println("He so luong: " + Wage);
+				// Xe
+				System.out.println("Ten Xe: " + carName);
+				System.out.println("Hang Xe: " + CarCompany);
+				System.out.println("Gia xe: " + valueCar);
+				System.out.println("Mau xe: " + Color);
 	}
 
 }
